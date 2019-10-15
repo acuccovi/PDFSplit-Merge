ad9e0cbd72425ab81ca9506ec0115100bf669eac
Alessio Cuccovillo <alessio.cuccovillo@gmail.com>
2019-10-15 15:34:09 +0200

    UPD parent pom.xml maven-compiler-plugin settings UPD module PDFSplitMerge module-info.java

--- cuccovillo.alessio.pdfsplitmerge/src/main/java/module-info.java
+++ cuccovillo.alessio.pdfsplitmerge/src/main/java/module-info.java
@@ -1,8 +1,5 @@
 module PDFSplitMerge {
     requires java.desktop;
-    requires org.apache.pdfbox;
-    requires org.apache.fontbox;
-    requires commons.logging;
 
     requires PDFSplitMergeLIB;
 }
